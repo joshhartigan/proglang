1fc99af44c01b9ff9e67b040b5eda061d7d3a050
joshhartigan <joshhartigan99@gmail.com>
2014-11-03 18:48:46 +0000

    handles negatives, is shorter/faster

--- infix.rb
+++ infix.rb
@@ -1,68 +1,35 @@
 # infix.rb: parse infix-operated math expressions
-
-def is_number?(str)
-  true if Float(str) rescue false
-end
+require 'scanf'
 
 class InfixParser
   @operations = [ '/', '*', '+', '-' ]
-
-  testString = '(24 + 6) / 10 * 3'
-  shouldEqual = 9
 
   def eval_expression(expr) #TODO: implement parsing of entire expressions
 
   end
 
-  def self.eval_binary_expression(chunk)
-    chunk = chunk.gsub ' ', ''
-
-    if not (chunk.include?('/') or chunk.include?('*') \
-        or chunk.include?('+') or chunk.include?('-'))
-        puts "error: no operations in chunk: #{chunk}"
-        exit
+  def self.eval_binary_expression(expr)
+    first_operand, oper, second_operand = expr.scanf('%f %c %f')
+    if first_operand.nil?
+      raise ArgumentError, 'missing or invalid left number'
     end
 
-    firstNumber = ''
-    secondNumber = ''
-
-    i = 0
-    currentChar = ''
-    while not @operations.include? currentChar
-      currentChar = chunk[i]
-      if not is_number? currentChar and not @operations.include? currentChar \
-         and currentChar != '.'
-        puts "error: non-numerical digit in chunk: #{currentChar}"
-        exit
+    begin
+      case oper
+        when '/' then first_operand / second_operand
+        when '*' then first_operand * second_operand
+        when '-' then first_operand - second_operand
+        when '+' then first_operand + second_operand
+        else raise ArgumentError, 'missing or invalid right number'
       end
-      firstNumber += currentChar
-      i += 1
+    rescue TypeError
+      raise ArgumentError, 'missing or invalid operation'
     end
-    firstNumber = firstNumber[0 .. -2]
-
-    for c in i-1 .. chunk.length-1
-      if not is_number? chunk[c] and not @operations.include? currentChar \
-         and currentChar != '.'
-        puts "error: non-numerical digit in chunk: #{currentChar}"
-        exit
-      end
-      secondNumber += chunk[c]
-    end
-    secondNumber = secondNumber[1 .. secondNumber.length - 1]
-
-    case chunk[i - 1]
-      when '/' then Float(firstNumber) / Float(secondNumber)
-      when '*' then Float(firstNumber) * Float(secondNumber)
-      when '+' then Float(firstNumber) + Float(secondNumber)
-      when '-' then Float(firstNumber) - Float(secondNumber)
-    end
-
   end
-
 end
 
 puts InfixParser.eval_binary_expression('433.45 * 2')
-puts InfixParser.eval_binary_expression('65*224.5')
-puts InfixParser.eval_binary_expression('4* 25')
+puts InfixParser.eval_binary_expression('-65*224.5')
+puts InfixParser.eval_binary_expression('4* -25')
 puts InfixParser.eval_binary_expression('489.67 *35')
 
