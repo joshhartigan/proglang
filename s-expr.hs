29c81f29f3785ca5cb210b6abb39a8971876d446
joshhartigan <joshhartigan99@gmail.com>
2014-12-12 20:46:31 +0000

    beginnings of s-expr parser in haskell

---
+++ s-expr.hs
@@ -0,0 +1,12 @@
+isParenthesised str = str !! 0 == '(' && str !! ((length str) - 1) == ')'
+
+slice from to xs = take (to - from + 1) (drop from xs)
+
+parseExpr expr
+  | not (isParenthesised expr) = error "s-expr.hs: expression isn't parenthesised"
+  | otherwise = words noParens
+  where noParens = slice 1 ((length expr) - 2) expr
+
+-- e.g. parseExpr "(+ (- 4 2) 5)" gives:
+-- ["+","(-","4","2)","5"]
+
