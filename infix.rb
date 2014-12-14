2d657841596095cf56c3934f04b99bf57b5bb674
joshhartigan <joshhartigan99@gmail.com>
2014-12-14 16:59:04 +0000

    begin rewrite of infix.rb to work fully

--- infix.rb
+++ infix.rb
@@ -1,35 +1,14 @@
-# infix.rb: parse infix-operated math expressions
-require 'scanf'
+#!/usr/bin/env ruby
 
-class InfixParser
-  @operations = [ '/', '*', '+', '-' ]
+def infix_to_prefix(expr)
+  operators = []
+  results = []
 
-  def eval_expression(expr) #TODO: implement parsing of entire expressions
+  for token in tokenise(expr)
+    if operator? token
 
-  end
-
-  def self.eval_binary_expression(expr)
-    first_operand, oper, second_operand = expr.scanf('%f %c %f')
-    if first_operand.nil?
-      raise ArgumentError, 'missing or invalid left number'
-    end
-
-    begin
-      case oper
-        when '/' then first_operand / second_operand
-        when '*' then first_operand * second_operand
-        when '-' then first_operand - second_operand
-        when '+' then first_operand + second_operand
-        else raise ArgumentError, 'missing or invalid right number'
-      end
-    rescue TypeError
-      raise ArgumentError, 'missing or invalid operation'
-    end
   end
 end
 
-puts InfixParser.eval_binary_expression('433.45 * 2')
-puts InfixParser.eval_binary_expression('-65*224.5')
-puts InfixParser.eval_binary_expression('4* -25')
-puts InfixParser.eval_binary_expression('489.67 *35')
+puts infix_to_prefix( ARGV.join ' ' ).join ' '
 
